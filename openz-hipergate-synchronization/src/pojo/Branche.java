2a88e29813ccb92ecce0a22af959268322cbbd57
cglaeser <glaeser@7says.de>
2014-11-16 18:43:26 +0100

    Branche is completed

--- openz-hipergate-synchronization/src/pojo/Branche.java
+++ openz-hipergate-synchronization/src/pojo/Branche.java
@@ -1,7 +1,68 @@
 package pojo;
 
+import java.util.Date;
+
+/**
+ * The minified version of c_bpartner_group table
+ * @author cglaeser
+ *
+ */
 public class Branche {
 	
+	private String ID;
 	
-
+	/**
+	 * An always identical number C726FEC915A54A0995C568555DA5BB3C
+	 */
+	private String ClientID;
+	
+	/**
+	 * An always identical number 5F1A818D54EF4182A30C9235FF8658E5
+	 */
+	private String OrgID;
+	
+	/**
+	 * An always identical number 1
+	 */
+	private boolean isActive;
+	
+	/**
+	 * The creation time of the entry
+	 */
+	private Date created;
+	
+	/**
+	 * An always identical number DDAA21D11CB04D4D8EC59E39934B27FB
+	 */
+	private String createdBy;
+	
+	/**
+	 * The last update time of the entry
+	 */
+	private Date updated;
+	
+	/**
+	 * An always identical number DDAA21D11CB04D4D8EC59E39934B27FB
+	 */
+	private String updatedBy;
+	
+	/**
+	 * The name of the Group;
+	 */
+	private String value;
+	
+	/**
+	 * Usually identical to name
+	 */
+	private String name;
+	
+	/**
+	 * The description of the group
+	 */
+	private String description;
+	
+	/**
+	 * Usually 'N'
+	 */
+	private char isDefault;
 }
--- openz-hipergate-synchronization/src/pojo/BusinessPartner.java
+++ openz-hipergate-synchronization/src/pojo/BusinessPartner.java
@@ -1,12 +1,55 @@
 package pojo;
 
+/**
+ * 
+ * The minified version of c_bp
+ * @author cglaeser
+ *
+ */
 public class BusinessPartner {
 	
 	private String businessPartnerKey;
 	private String name;
-	private String taxNumber;
 	private String name2;
+	private String taxNumber;
 	private String description;
 	private String Branche;
+	
+	public String getBusinessPartnerKey() {
+		return businessPartnerKey;
+	}
+	public void setBusinessPartnerKey(String businessPartnerKey) {
+		this.businessPartnerKey = businessPartnerKey;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+	public String getName2() {
+		return name2;
+	}
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+	public String getBranche() {
+		return Branche;
+	}
+	public void setBranche(String branche) {
+		Branche = branche;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
