5743e2ba6b3f7a0661886d17ff1b467274850e94
cglaeser <glaeser@7says.de>
2014-11-16 18:26:05 +0100

    Initial commit

---
+++ openz-hipergate-synchronization/src/io/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author HAWAI
+ *
+ */
+package io;
---
+++ openz-hipergate-synchronization/src/pojo/BusinessPartner.java
@@ -0,0 +1,12 @@
+package pojo;
+
+public class BusinessPartner {
+	
+	private String businessPartnerKey;
+	private String name;
+	private String taxNumber;
+	private String name2;
+	private String description;
+	private String Branche;
+
+}
