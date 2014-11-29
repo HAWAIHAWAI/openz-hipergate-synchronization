e8a65af914dcd3507a82941f5c0b9c49bf2c4d52
cglaeser <glaeser@7says.de>
2014-11-29 18:41:49 +0100

    Some more changes and extended package structure

---
+++ openz-hipergate-synchronization/src/io/xml/BusinessPartnerXML.java
@@ -0,0 +1,19 @@
+package io.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import pojo.BusinessPartner;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+public class BusinessPartnerXML {
+	
+	public static String getBusinessPartnerByID(String id){
+	XStream xstream = new XStream(new DomDriver());
+	//xstream.alias("billNumber", String.class);
+	return xstream.toXML(database.BusinessPartnerUtil.getBusinessPartner(id));
+	}
+}
